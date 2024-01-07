62445733caea8dd9077b72361e05be467f822910
hannemanrichard <104073700+hannemanrichard@users.noreply.github.com>
2024-01-07 23:39:15 +0100

    change pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     import("react-facebook-pixel")
       .then((x) => x.default)
       .then((ReactPixel) => {
-        ReactPixel.init("1812704482494462"); // facebookPixelId
+        ReactPixel.init("658771986439198"); // facebookPixelId
         ReactPixel.pageView();
 
         router.events.on("routeChangeComplete", () => {
